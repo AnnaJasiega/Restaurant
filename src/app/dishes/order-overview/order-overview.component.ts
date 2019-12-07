92d29ce8a7f6f26b75bc3dacf8a15c9cf3b6ba99
Anna Jasiega <ajasiega@gmail.com>
2019-12-07 15:04:50 +0100

    Restaurant menu

--- src/app/dishes/order-overview/order-overview.component.ts
+++ src/app/dishes/order-overview/order-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,  } from '@angular/core';
 import { DishesComponent } from '../dishes-overview/dishes.component';
 import { Dish } from '../dish';
 
@@ -10,22 +10,16 @@ import { Dish } from '../dish';
 })
 export class OrderOverviewComponent implements OnInit {
 
-  total: Number = 0;
-  
+    
   @Input('orderList') orderList: Array<Dish>;
+  @Input('total') total: number;
 
-  totalPrice(){
-    for (let i=0; i<this.orderList.length; i++){
-      this.total =+ this.orderList[i].$price
-    }
-    return this.total
-  }
 
   constructor() { }
   
  
   ngOnInit() {
-   this.totalPrice();
+   
     
   }
 
